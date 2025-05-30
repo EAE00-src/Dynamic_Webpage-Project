3838f5f00fa8dafdb18b52a1f017be7cd7e0995c
EAE00-src <edwin.edouard00@gmail.com>
2025-05-30 18:22:41 -0400

    Let's say this is V0.3; -I made both sure fetch functions work -Event listeners tweaked to include fetched data arrays -Loops within event listeners help filter arrays --Next step is to get the event listeners to either alter the innHTML or displays of images dynamically

--- FrontEnd/assets/app.js
+++ FrontEnd/assets/app.js
@@ -1,4 +1,4 @@
-// api link: 'http://localhost:5678/api/categories'
+// api link: 'http://localhost:5678/api/'
 console.log("app.js")
 /*fetch('http://localhost:5678/api/categories')
     .then((response) => response.json())
@@ -8,19 +8,122 @@ console.log("app.js")
     .catch((err) => {
         console.error(`There was an error: ${err}`)
     });*/
-    async function getData() {
+
+// fetch functions
+async function getCateg() {
 	const apiCateg = 'http://localhost:5678/api/categories';
 		try {
 		const response = await fetch(apiCateg);
 			if (!response.ok){
 				throw new Error(`Response status: ${response.status}`)
-			}
+			}else{
 			
-            const json = await response.json();
-            console.log(json);
+            const categData = await response.json();
+			return categData;
+			};
 	     }catch (error) {
             console.error(error.message);
 		  }
 	};
-getData();
-console.log(getData());
+
+//testing json data reference outside of function
+/*getCateg().then(categories => {
+  console.log(categories); // Access the first category's id
+  console.log(categories[1].name); // Access the first category's name
+}).catch(error => {
+  console.error(error.message);
+});*/
+
+
+
+async function getWorks() {
+	const apiWorks = 'http://localhost:5678/api/works';
+		try {
+		const response = await fetch(apiWorks);
+			if (!response.ok){
+				throw new Error(`Response status: ${response.status}`)
+			}
+			
+            const works = await response.json();
+            console.log(works);
+			return works;
+	     }catch (error) {
+            console.error(error.message);
+		  }
+	};
+
+
+getCateg();
+getWorks();
+
+
+
+/*getWorks().then(objects =>{
+	console.log(objects[4].category.id);
+}).catch(error =>{
+	console.error(error);
+});*/
+
+
+//button variables
+const filters = document.getElementsByClassName('filters');
+const filterAll = document.getElementById('b-All');
+const filterObj = document.getElementById('b-Obj');
+const filterApt = document.getElementById('b-Apt');
+const filterVen = document.getElementById('b-Ven');
+let gallery = document.getElementsByClassName('gallery');
+let objGallery = document.getElementsByClassName('obj');
+
+//Event listeners (filters)
+filterAll.addEventListener('click', () =>{
+	getWorks().then(works =>{
+		let allResults = [];
+		for (let all of works){
+			console.log(all);
+		};
+		
+	});
+});
+
+//objects filter
+filterObj.addEventListener('click', () =>{
+	getWorks().then(obj =>{
+		let results = [];
+		for (let prop of obj){
+			if(prop.category.name === 'Objects'){
+				console.log(prop);
+				results.push(prop);
+			}
+	
+		};
+				console.log(results);
+		});
+	});
+
+//apartments filter
+filterApt.addEventListener('click', () =>{
+	getWorks().then(apartments => {
+		let aptResults = [];
+		for(let apartment of apartments){
+			if(apartment.category.name === 'Apartments'){
+				aptResults.push(apartment);
+				
+			}
+		};
+		console.log(aptResults);
+	});
+});
+
+//hotels & restaurants filter
+filterVen.addEventListener('click', () =>{
+	getWorks().then(restaurants => {
+		let hrResults = [];
+		for(let prop of restaurants){
+			if(prop.category.name === 'Hotels & restaurants'){
+				hrResults.push(prop);
+				
+			}
+		};
+		console.log(hrResults);
+	});
+});
